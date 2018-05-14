2a055475c5cbce70f904cbf8efeb94d3becb014c
Aaron <aaron.thomas.harding@googlemail.com>
2018-05-14 22:00:36 +0200

    add in footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Footer.block.css'
+
+const Footer = () => {
+    return (
+        <div className={styles}>footer</div>
+    )
+}
+
+export default Footer
