5b99b7ccbf070381c5f29617168a6a1ddd13e17f
Aaron <aaron@mycujoo.tv>
2018-05-17 17:20:43 +0200

    fix css-blocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Footer from './components/Footer'
+import Footer from './components/Footer/index.jsx';
 
 class App extends Component {
   render() {
